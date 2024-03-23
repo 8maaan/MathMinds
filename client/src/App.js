1b387969200dcd3caa33cd4e3f8564e2747135b9
8maaan <johnrandolf696969@gmail.com>
2024-03-23 22:53:13 +0800

    Create login branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import LoginPage from './Pages/LoginPage'
+import  {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <p>Routes here</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
