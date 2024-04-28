e15cae4fc2249e4861f5ee0f67e57f0a433a1f1a
John Rodolph Bacalso <johnradbacalso@gmail.com>
2024-04-28 13:13:44 +0800

    Updated the js and css of landing page

--- client/src/Pages/LandingPage.js
+++ client/src/Pages/LandingPage.js
@@ -11,37 +11,44 @@ const LandingPage = () => {
     return (
         <div className='body'>
             <div className='landing-nav'>
+                <div className='mobilelogo-and-rightbutton-container'>
                 <div className='mobile-view-logo'>
                     <img src={mathMindsHorizontalLogo} alt='Logo'/>
                 </div>
                 <div className='right-buttons'>
-                    <button style={{backgroundColor: '#ffffff'}} className='button' onClick={() => navigate('/register')}>SIGN UP</button>
-                    <button className='button' onClick={() => navigate('/login')}>LOG IN</button>
+                    <button style={{backgroundColor: '#ffffff'}} className='regbutton' onClick={() => navigate('/register')}>SIGN UP</button>
+                    <button className='logbutton' onClick={() => navigate('/login')}>LOG IN</button>
+                </div>
                 </div>
             </div>
 
-            <div>
+            <div className='landing-container'>
                 <div className='landing-body-grid-container'>
+                    <div className='container-text-and-image'>
                     <div className='text-section'>
+                        <div className='first-text'>
                         <h1>
                             <span>We Make</span><br/>
                             <span>Mathematics</span><br/>
                             <span>Fun.</span>
                         </h1>
+                        </div>
+                        <div className='second-text'>
                         <p>We transform the study of mathematics into a wonderful<br/>
                         experience through interactive lessons, creative<br/>
                         problem-solving, and real-world applications.</p>
+                        </div>
                     </div>
                     <div className='image-section'>
+                        <div className='titser'>
                         <img src={landingBodyImage} alt='Teacher'/>
-                        <button onClick={() => navigate('login')} className='button'>GET STARTED</button>
+                        </div>
+                        <div className='getstartedbutton'>
+                        <button onClick={() => navigate('login')} className='gsbutton'>GET STARTED</button>
+                        </div>
+                    </div>
                     </div>
                 </div>
-            </div>
-
-            <div className='mobile-view-buttons'>
-                <button style={{backgroundColor: '#ffffff'}}className='button' onClick={() => navigate('/register')}>SIGN UP</button>
-                <button className='button' onClick={() => navigate('/login')}>LOG IN</button>
             </div>
         </div>
     )
