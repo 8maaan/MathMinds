554b4ca76f76c3c169ae4a73c960596238cd4f48
JohnRodolphBacalso <johnrodolph.bacalso@cit.edu>
2024-05-04 00:30:01 +0800

    Added Profile Page js and css

--- client/src/Pages/ProfilePage.js
+++ client/src/Pages/ProfilePage.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { UserAuth } from '../Context-and-routes/AuthContext'
-import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import '../PagesCSS/ProfilePage.css';
+import ReusableAppBar from '../ReusableComponents/ReusableAppBar';
+import userprofilepic from '../Images/UserDP.png';
+import { Button } from '@mui/material';
 
 // FOR TESTING PURPOSES ONLY (ROUTES)
 // EDIT LATER
@@ -22,10 +25,46 @@ const HomePage = () => {
 
     return (
         <div>
-            {/* TESTING ONLY, TO BE REMOVED L8ER */}
-            {user ? <p>Hello {user.email} </p> : <p>Not found user</p>}
-            <Button variant='contained' size='large' onClick={handleSignOut}> SIGN OUT</Button>
+             <div class="Profilepage">
+             <ReusableAppBar/>
+                <div className='profile-wrapper'>
+                    <div className='profile-content-container'>
+                        <div className='personalinfo-left-side'>
+                                    <div className='choices-container'>
+                                        <div className='PI-button'>  
+                                        Personal Information
+                                    </div>
+                                    <div className='LessonProg-button'>                          
+                                        Lesson Progress
+                                    </div>
+                                    <div className='Badges-button'>
+                                        Badges
+                                    </div>
+                                    </div>
+                        </div>
+                        <div className='personalinfo-right-side'>
+                                    <div className='PI-container'>
+                                    <div className='PI-title'>PERSONAL INFORMATION
+                                    </div>
+                                    <div className='logo-and-userinfo-container'>
+                                    <div className='profile-logo-container'>
+                                    <img src={userprofilepic} alt='logo'/>
+                                    </div>
+                                    <div className='userinfo-container'>
+                                    </div>
+                                    </div>
+
+                                    
+                                    </div>
+                            
+                            
+                        </div>
+
+                    </div>
+                </div>  
+            </div>
         </div>
+        
     )
 }
 
