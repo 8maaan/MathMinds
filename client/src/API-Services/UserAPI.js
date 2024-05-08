1c237d5418633eef158fd3e0f5dfe0220404e6ff
JohnRodolphBacalso <johnrodolph.bacalso@cit.edu>
2024-05-08 22:43:26 +0800

    Updated the getUserProfileInfo function

--- client/src/API-Services/UserAPI.js
+++ client/src/API-Services/UserAPI.js
@@ -12,12 +12,12 @@ export const createUserToDb = async(user) => {
 
 
 //GET USER PROFILE INFO
-export const getUserProfileInfo = async(uid) =>{
-    try{
-        const response = await axios.get(`http://localhost:8080/mathminds/user/getUserProfileInfo/${uid}`);
-        return{user: response.data, success:true,message:'Successfully fetch User Info.'}
-    }catch(error){
-        console.error("Error: ", error);
-        return {success: false, message: error}
+export const getUserProfileInfo = async (uid) => {
+    try {
+        const response = await axios.get(process.env.REACT_APP_SPRINGBOOT_GET_USER_PROFILE_INFO,uid );
+        return { success: true, data: response.data };
+    } catch (error) {
+        console.error("Error", error);
+        return { success: false, message: error};
     }
-}
+};
--- client/src/Pages/ProfilePage.js
+++ client/src/Pages/ProfilePage.js
@@ -5,113 +5,110 @@ import '../PagesCSS/ProfilePage.css';
 import ReusableAppBar from '../ReusableComponents/ReusableAppBar';
 import userprofilepic from '../Images/UserDP.png';
 import { Button, TextField } from '@mui/material';
-import { getUserProfileInfo } from '../API-Services/UserAPI';
-
-// FOR TESTING PURPOSES ONLY (ROUTES)
-// EDIT LATER
-
-const HomePage = () => {
+import { getUserProfileInfo } from '../API-Services/UserAPI'; 
+const ProfilePage = () => {
     const { user, logOut } = UserAuth();
-    const[userinfo,setUserInfo]=useState([]);
-
+    const [userinfo, setUserInfo] = useState({
+        fname: "",
+        lname: "",
+        email: ""
+    });
     const navigateTo = useNavigate();
 
-    const handleSignOut = async () =>{
-        try{
+    useEffect(() => {
+       
+        const fetchUserProfileInfo = async () => {
+            try {
+                const response = await getUserProfileInfo(user.uid); 
+                if (response.success) {
+                    const { fname, lname, email } = response.data; 
+                    setUserInfo({ fname, lname, email }); 
+                } else {
+                    console.error("Failed to fetch user profile info:", response.error);
+                }
+            } catch (error) {
+                console.error("Error fetching user profile info:", error);
+            }
+        };
+
+        fetchUserProfileInfo(); 
+    }, [user.uid]); 
+
+    const handleSignOut = async () => {
+        try {
             await logOut();
             console.log('You are logged out');
             navigateTo('/login');
-        }catch (e){
+        } catch (e) {
             console.log(e.message);
         }
     }
 
-
-        useEffect(()=>{
-            const getUserInfo = async () =>{
-                const result = await getUserProfileInfo(localStorage.getItem("uid"));
-                if(result.success){
-                    setUserInfo(result.user);
-                }else{
-                    console.log(result.message);
-                }
-            };
-            getUserInfo();
-        },[]);
-
-        const InfoTextField=({label,defaultValue})=>{
-            return(
-                <div className="profile-info-texts">
-                    
-                    <div className="profile-info-textfields">
-                        <TextField
-
-                        disabled
-                        defaultValue={defaultValue}
-                        size="large"
-                        className="customTextField"
-                        label={<span>{label}<span style={{color: 'black'}}> *</span></span>}
-                        />
-                    </div>
-                </div>
-            );
-        };
-
     return (
         <div>
-             <div class="Profilepage">
-             <ReusableAppBar/>
+            <div className="Profilepage">
+                <ReusableAppBar />
                 <div className='profile-wrapper'>
                     <div className='profile-content-container'>
                         <div className='personalinfo-left-side'>
-                                    <div className='choices-container'>
-                                        <div className='PI-button'>  
-                                        Personal Information
-                                    </div>
-                                    <div className='LessonProg-button'>                          
-                                        Lesson Progress
-                                    </div>
-                                    <div className='Badges-button'>
-                                        Badges
-                                    </div>
-                                    </div>
+                            <div className='choices-container'>
+                                <div className='PI-button'>
+                                    Personal Information
+                                </div>
+                                <div className='LessonProg-button'>
+                                    Lesson Progress
+                                </div>
+                                <div className='Badges-button'>
+                                    Badges
+                                </div>
+                            </div>
                         </div>
                         <div className='personalinfo-right-side'>
-                                    <div className='PI-container'>
-                                    <div className='PI-title'>PERSONAL INFORMATION
-                                    </div>
-                                    <div className='logo-and-userinfo-container'>
+                            <div className='PI-container'>
+                                <div className='PI-title'>PERSONAL INFORMATION</div>
+                                <div className='logo-and-userinfo-container'>
                                     <div className='profile-logo-container'>
-                                    <img src={userprofilepic} alt='logo'/>
+                                        
+                                        <img src={userprofilepic} alt='logo' />
                                     </div>
                                     <div className='userinfo-container'>
-                                        <InfoTextField
-                                        label='FIRSTNAME'
-                                        defaultValue={userinfo.fname}
-                                        />
-                                         <InfoTextField
-                                       label='LASTNAME'
-                                        defaultValue={userinfo.lname}
-                                        />
-                                         <InfoTextField
-                                        label='EMAIL'
-                                        defaultValue={userinfo.email}
-                                        />
-
-                                    </div>
-                                    </div>
-
+                      
+                                        <div>
+                                            <TextField
+                                                label="First Name"
+                                                value={userinfo.fname}
+                                                InputProps={{
+                                                    readOnly: true,
+                                                }}
+                                            />
                                         </div>
-                            
-                            
+                                        <div>
+                                            <TextField
+                                                label="Last Name"
+                                                value={userinfo.lname}
+                                                InputProps={{
+                                                    readOnly: true,
+                                                }}
+                                            />
+                                        </div>
+                                        <div>
+                                            <TextField
+                                                label="Email"
+                                                value={userinfo.email}
+                                                InputProps={{
+                                                    readOnly: true,
+                                                }}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-
                     </div>
-                </div>  
+                </div>
             </div>
         </div>
-        
     )
 }
 
-export default HomePage
+export default ProfilePage;
