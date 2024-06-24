6435bdf94a8231c6af6624ad3d4c4c241b49021b
dedensyaaa <denisseclaire.morales@cit.edu>
2024-06-25 02:15:15 +0800

    made slight changes to home page and topics page

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,36 +1,40 @@
 import React from 'react'
 import '../PagesCSS/HomePage.css'
-import {Box,  Typography, Button, } from '@mui/material'
-import { useNavigate } from 'react-router-dom';
+import {Box, Typography, Button} from '@mui/material'
+import { Link } from 'react-router-dom';
 import homepageDashboardBtn from '../Images/HomePage_DashboardBtn.png';
 import homepageLessonBtn from '../Images/HomePage_LessonsBtn.png';
 import homepagePracticeBtn from '../Images/HomePage_PracticeBtn.png';
 
 const HomePage = () => {
-  const navigateTo = useNavigate();
-
     return (
         <div>
-            <div class="Homepage">
+            <div className="Homepage">
                 <Box>
-                    <Typography class='home-header'>
+                    <Typography class="home-header">
                         Hey there, math explorer!
                     </Typography>
-                    <Typography class='home-paragraph'>
+                    <Typography class="home-paragraph">
                         Ready to dive into the world of numbers and have some math fun together?
                     </Typography>
                 </Box>
 
-                <Box className='image-buttons'>
-                    <Button className='image-buttons-margin'onClick={()=> navigateTo('/profile')}>
-                        <img src={homepageDashboardBtn} alt="Dashboard" className='img-button-size'/>
-                    </Button>
-                    <Button className='image-buttons-margin' onClick={()=> navigateTo('/lessons')} sx={{marginLeft: 3, marginRight: 3}}>
-                        <img src={homepageLessonBtn} alt="Lessons" className='img-button-size' />
-                    </Button>
-                    <Button className='image-buttons-margin'onClick={()=> navigateTo('/practice')}>
-                        <img src={homepagePracticeBtn} alt="Practice" className='img-button-size' />
-                    </Button>
+                <Box className="image-buttons">
+                    <Link to="/profile" className="image-buttons-margin">
+                        <Button className="img-button-size">
+                            <img src={homepageDashboardBtn} alt="Dashboard" className="img-button-size" />
+                        </Button>
+                    </Link>
+                    <Link to="/lessons" className="image-buttons-margin" style={{ marginLeft: '24px', marginRight: '24px' }}>
+                        <Button className="img-button-size">
+                            <img src={homepageLessonBtn} alt="Lessons" className="img-button-size" />
+                        </Button>
+                    </Link>
+                    <Link to="/practice" className="image-buttons-margin">
+                        <Button className="img-button-size">
+                            <img src={homepagePracticeBtn} alt="Practice" className="img-button-size" />
+                        </Button>
+                    </Link>
                 </Box>
             </div>       
         </div>
