d33330a8f03c8f5dd9a6c57d79ea4e2c483effcc
dabeondona <ondonadave5@gmail.com>
2024-09-23 21:43:02 +0800

    Removed AOS initalization in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import AOS from 'aos';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-AOS.init();
 
 root.render(
   <React.StrictMode>
